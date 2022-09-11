d55b88fca8278ca2bfd1833196800fc26f3f20bf
Badari-Narayana-k <95167800+Badari-Narayana-k@users.noreply.github.com>
2022-09-11 21:46:34 +0530

    adding files regarding the blueprint for the website
    this commit is regarding the files for the blueprint of the website which describes about the login page,cover page and features page of our project.

---
+++ pfsd_646/sdp1/app.py
@@ -0,0 +1,23 @@
+from flask import *
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('coverpage.html')
+
+
+@app.route('/login',)
+def login():
+    return render_template('index.html')
+
+
+@app.route('/album')
+def contact():
+    return render_template('album.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
